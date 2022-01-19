c8385e89a5acac4a2a72c4210a2b397852d32609
Alan Poveda <alsepoveda@gmail.com>
2022-01-19 20:10:50 -0300

    Made style Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default class App extends Document {
                         href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap"
                         rel="stylesheet"
                     />
-
+                    <link rel='shortcut icon' href='/favicon.png' type='image/png'></link>
                 </Head>
                 <body>
                     <Main />
