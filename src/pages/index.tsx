ff22922fd927f6a46e8ee1b848cde2d57f64213a
Alan Poveda <alsepoveda@gmail.com>
2022-01-20 20:35:28 -0300

    Page home, css complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
     <Head>
       <title>ignews</title>
     </Head>
-    <div>
-     <h1>Hello Ignews</h1>
-    </div>
+
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>🙋🏽‍♂️ Hey, welcome</span>
+        <h1>New about the <span>React</span> world</h1>
+        <p>
+          Get access to all the publications <br/>
+          <span> for $1.90 month </span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />
+
+    </main>
+
+    
     </>
     
   )
