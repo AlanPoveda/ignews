2f803ac44076f1579db9d52243226f88c18ff131
Alan Poveda <alsepoveda@gmail.com>
2022-01-24 21:12:13 -0300

    Upload the request the api price for SSG, for best perform

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { stripe } from '../services/stripe'
 import { SubscribeButton } from '../components/SubscribeButton'
@@ -44,7 +44,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () =>{
+export const getStaticProps: GetStaticProps = async () =>{
 
   const price = await stripe.prices.retrieve('price_1KKW6zAV19k8lLPsjd5pHrlY')
 
@@ -59,6 +59,7 @@ export const getServerSideProps: GetServerSideProps = async () =>{
   return{
     props: {
       product
-    }
+    },
+    revalidate: 60*60*24, // 24 hrs
   }
 }
