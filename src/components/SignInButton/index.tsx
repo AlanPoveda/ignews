910293e65874df8ca42d28c40b9d852100a25006
Alan Poveda <alsepoveda@gmail.com>
2022-02-01 19:57:17 -0300

    Create a stripe checkout and not duplicate informations. Save the stripe id in database

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,32 @@
 import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi"
-import { signIn, signOut , useSession } from 'next-auth/react'
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 
-
 export function SignInButton() {
-    const { data: session } = useSession()
-    console.log(session)
+    const { data: session } = useSession();
 
     return session ? (
         <>
-            <button className={styles.singInButton} type="button" onClick={() => signOut()}>
+            <button
+                className={styles.singInButton}
+                type="button"
+                onClick={() => signOut()}
+            >
                 <FaGithub color="#04d361" />
                 {session.user.name}
-                <FiX color='#737380' className={styles.closeIcon} />
+                <FiX color="#737380" className={styles.closeIcon} />
             </button>
         </>
     ) : (
         <>
-            <button className={styles.singInButton} type="button" onClick={() => signIn('github')}>
+            <button
+                className={styles.singInButton}
+                type="button"
+                onClick={() => signIn("github")}
+            >
                 <FaGithub color="#eba417" />
-                Sign in whit GitHub
+                Sign in with GitHub
             </button>
         </>
     );
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import { stripe } from '../../services/stripe'
+import { fauna } from '../../services/fauna'
+import { query as q } from 'faunadb'
+
+
+type User = {
+    ref: {
+        id: string;
+    }
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async function SubscriptRequestSession(req: NextApiRequest, res: NextApiResponse) {
 
@@ -10,12 +22,48 @@ export default async function SubscriptRequestSession(req: NextApiRequest, res: 
         //Pegando a info da pessoa logada nos cookies, que é única forma de poder pegar a informação no back-end
         const session = await getSession({ req })
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email
-        })
+        //Parar não duplicar, será necessário fazer uma query no fauna para ver o user se já existe
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        //Pega o id do stripe, se existe ou vazio
+        let customerId = user.data.stripe_customer_id
+
+
+        //Verificando se existe, se não, ele cria
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email
+            })
+
+
+            //Necessário fazer a tipagem pra o type User, por causa do ref, ainda salvando o id do
+            //Custumer logado, salvando o seu id
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+
+        }
+
+
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{
