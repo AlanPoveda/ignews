2ea3ff5409fe7bfd377b346bcda7115d579ee881
Alan Poveda <alsepoveda@gmail.com>
2022-01-31 23:06:24 -0300

    Make a login and save de email in database, using a Faundadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,4 +18,24 @@ export default NextAuth({
       }
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile } ){
+
+      const { email } = user;
+
+      try{
+        await fauna.query(
+          query.Create(
+            query.Collection('users'),
+            { data: { email }}
+          )
+        )
+        return true
+      }catch{
+        return false
+      }
+      
+    },
+  }
 })
