d8df31b4d0c0d841bdde484a11c2e1e371c7651a
Alan Poveda <alsepoveda@gmail.com>
2022-02-01 22:23:08 -0300

    Testing a payament method, and see te webhooks in local, and see the most relevant events for payament method

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,63 @@
+import { NextApiResponse, NextApiRequest } from "next"
+import Stripe from 'stripe'
+import { stripe } from '../../services/stripe'
+
+//Stream
+import { Readable } from 'stream'
+
+//Funcao para ler as streams
+
+async function buffer(readable: Readable) {
+    const chunks = [];
+
+    for await (const chunk of readable) {
+        chunks.push(
+            typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+        );
+    }
+
+    return Buffer.concat(chunks)
+}
+
+//Desabilitar o entendimento padrão do react, pois não é um Json que esta recebendo, mas sim uma stream
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+//Pegando os valores relevantes
+const relevantEvents = new Set([
+    "checkout.session.completed"
+])
+
+export default async function webHookResponse(req: NextApiRequest, res: NextApiResponse) {
+
+    if (req.method === "POST") {
+        const buff = await buffer(req);
+        //Pegando a chave do header, que também é a mesma da env
+        const secret = req.headers['stripe-signature']
+
+        let event: Stripe.Event;
+
+
+        try {
+            event = stripe.webhooks.constructEvent(buff, secret, process.env.STRIPE_WEBHOOK_SECRET_KEY)
+
+        } catch (err) {
+            return res.status(400).send(`Webhook error ${err}`)
+        }
+
+        const { type } = event;
+
+        if(relevantEvents.has(type)){
+            console.log('Évento recebido ', event)
+        }
+
+        res.json({ 'message': 'OK' })
+    } else {
+        res.setHeader("Allow", "POST")
+        res.status(405).end("Method not allowed")
+    }
+}
