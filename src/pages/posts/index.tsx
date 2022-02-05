184a4692376dfa2654a8566397f0dec265c05cc3
Alan Poveda <alsepoveda@gmail.com>
2022-02-05 16:45:10 -0300

    Make the right format in getStaticProps and visualize in the component

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,13 +1,23 @@
-import styles from './styles.module.scss';
-import Head from 'next/head';
-import { GetStaticProps } from 'next';
-import { getPrismicClient } from '../../services/prismic';
-import Prismic from '@prismicio/client' 
+import styles from "./styles.module.scss";
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
+type Post = {
+    slug: string;
+    title: string;
+    excerpt: string;
+    updatedAt: string;
+};
 
+interface PostProps {
+    posts: Post[];
+}
 
-export default function Posts(){
-    return(
+export default function Posts({ posts }: PostProps) {
+    return (
         <>
             <Head>
                 <title>Post ignews</title>
@@ -15,49 +25,54 @@ export default function Posts(){
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>02 de março de 2022</time>
-                        <strong>Como criar algo do zero</strong>
-                        <p>Parar criar algo do zero, tem que começar pelo zero, então depois disso vai começar, ai para de ser do zero</p>
-                    </a>
-                    <a href="#">
-                        <time>02 de março de 2022</time>
-                        <strong>Como criar algo do zero</strong>
-                        <p>Parar criar algo do zero, tem que começar pelo zero, então depois disso vai começar, ai para de ser do zero</p>
-                    </a>
-                    <a href='#'>
-                        <time>02 de março de 2022</time>
-                        <strong>Como criar algo do zero</strong>
-                        <p>Parar criar algo do zero, tem que começar pelo zero, então depois disso vai começar, ai para de ser do zero</p>
-                    </a>
-                    <a href='#'>
-                        <time>02 de março de 2022</time>
-                        <strong>Como criar algo do zero</strong>
-                        <p>Parar criar algo do zero, tem que começar pelo zero, então depois disso vai começar, ai para de ser do zero</p>
-                    </a>
+                    {posts.map((post) => (
+                        <a href={post.slug} key={post.slug}>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
     );
-
 }
-
 
 //Chamada da api apra carregar a página uma unica vez, ainda mais para não consumir a banda
 export const getStaticProps: GetStaticProps = async () => {
+    const prismic = await getPrismicClient();
 
-    const prismic = await getPrismicClient()
+    const response = await prismic.query<any>(
+        [Prismic.predicates.at("document.type", "post")],
+        {
+            fetch: ["title", "content"],
+            pageSize: 100,
+        },
+    );
 
-    const response = await prismic.query([
-        Prismic.predicates.at('document.type', 'post' )
-    ], {
-        fetch: ['title', 'content'],
-        pageSize: 100
-    })
-
-    console.log(JSON.stringify(response, null, 2))
+    //Formatar o resultado vindo pela api pelo backend antes de enviar para o component
+    const posts = response.results.map((post) => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt:
+                post.data.content.find(
+                    (content) => content.type === "paragraph",
+                )?.text ?? "",
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+                "pt-BR",
+                {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                },
+            ),
+        };
+    });
 
     return {
-        props: {}
-    }
-}
+        props: {
+            posts,
+        },
+    };
+};
